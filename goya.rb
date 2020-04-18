dbdaffc380f2b38dc07d8bd778d838739fa54b11
sadaking <sichirigahama.37@gmail.com>
2020-04-18 17:35:49 +0900

    web technology task

---
+++ goya.rb
@@ -0,0 +1,41 @@
+# データを受け取り、返却するCGIプログラムの起動
+require 'cgi'
+cgi = CGI.new
+# データを受け取った後、HTMLの形式でレスポンスを返す
+if cgi.include?('goya')
+  cgi.out("type" => "text/html", "charset" => "UTF-8") {
+    # 情報の目印とした"goya"のデータを cgi['goya']と言う記述で取り出し、ローカル変数に代入する
+    get = cgi['goya']
+    # HTMLでレスポンスを返却する
+      "<html>
+        <body>
+          <p>ゴーヤの大きさと売った相手の情報は下記になります</p>
+          文字列：#{get}
+        </body>
+      </html>"
+  }
+elsif cgi.include?('goya1')
+  cgi.out("type" => "text/html", "charset" => "UTF-8") {
+    # 情報の目印とした"goya"のデータを cgi['goya']と言う記述で取り出し、ローカル変数に代入する
+    get = cgi['goya1']
+    # HTMLでレスポンスを返却する
+      "<html>
+        <body>
+          <p>譲渡先が自家消費ではないもののgoyaの情報は下記になります</p>
+          文字列：#{get}
+        </body>
+      </html>"
+  }
+elsif cgi.include?('goya2')
+  cgi.out("type" => "text/html", "charset" => "UTF-8") {
+    # 情報の目印とした"goya"のデータを cgi['goya']と言う記述で取り出し、ローカル変数に代入する
+    get = cgi['goya2']
+    # HTMLでレスポンスを返却する
+      "<html>
+        <body>
+          <p>品質が悪いもの（false）のgoyaの情報は下記になります</p>
+          文字列：#{get}
+        </body>
+      </html>"
+  }
+end
